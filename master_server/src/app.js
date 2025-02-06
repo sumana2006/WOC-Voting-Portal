69695641b6830f1b4308336346be175cac26ead4
AadityaSharma1001 <b23me1001@iitj.ac.in>
2025-02-07 01:58:09 +0530

    added routes for registration of voter, staff and volunteer

--- master_server/src/app.js
+++ master_server/src/app.js
@@ -2,7 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 // import routes
-import registrationRouter from "./routes/registration.js"
+import registrationRouter from "./routes/registration.routes.js"
+import voterRegistrationRouter from "./routes/voter_registration.routes.js"
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.get('/', (req, res) => {
     res.send('Hello World bye');
   });
 
-app.use("/api/register",registrationRouter)
+app.use("/api/register",registrationRouter);
+app.use("/api/voter",voterRegistrationRouter);
+
 
 export default app;
