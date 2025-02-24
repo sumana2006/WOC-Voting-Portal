01dc55e1622fd9371691b48fd3f7205625f4808f
Aradhya2708 <aradhyamahajan2708@gmail.com>
2025-02-25 04:19:50 +0530

    add vote casting and session routes

--- master_server/src/routes/voting.routes.js
+++ master_server/src/routes/voting.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
+import { decryptMiddleware } from "../middlewares/decryption.middleware";
 
 const router = express.Router();
 
 // router.get("/eligible-positions", voterAuthentication, getEligibleCandidates);
 // router.post("/voter-commitment", voterAuthentication, storingCommitment);
+
+router.post('/login', decryptMiddleware, handleVoterSession);
 
 export default router;
