3842a44feba692f55826ebcffc306f368e99aad4
Aradhya2708 <aradhyamahajan2708@gmail.com>
2025-02-25 18:32:58 +0530

    add positions enum

---
+++ master_server/src/constants/positions.js
@@ -0,0 +1,7 @@
+export const POSITIONS = {
+    GENSECY_SS: "general_secretary_ss",
+    GENSECY_SAC: "general_secretary_sas",
+    GENSECY_ACAC: "general_secretary_acac",
+    VP_BIE: "vice_president_bie",
+    VP_BCCA: "vice_president_bcca",
+};
