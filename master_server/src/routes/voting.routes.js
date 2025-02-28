34247c04c993632376908174bd3b9bb3ce2f32b8
Aradhya2708 <aradhyamahajan2708@gmail.com>
2025-03-01 01:47:00 +0530

    use voting middleware

--- master_server/src/routes/voting.routes.js
+++ master_server/src/routes/voting.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { decryptMiddleware } from "../middlewares/decryption.middleware.js";
 import { handleCastVote, handleVoterSession } from "../controllers/voting.controllers.js";
+import { sessionValidationMW } from "../middlewares/voting.middleware.js";
 
 const router = express.Router();
 
@@ -8,6 +9,6 @@ const router = express.Router();
 // router.post("/voter-commitment", voterAuthentication, storingCommitment);
 
 router.post('/login', decryptMiddleware, handleVoterSession);
-router.post('/cast', decryptMiddleware, handleCastVote)
+router.post('/cast', decryptMiddleware, sessionValidationMW, handleCastVote)
 
 export default router;
