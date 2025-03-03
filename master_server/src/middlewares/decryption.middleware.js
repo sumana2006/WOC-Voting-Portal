4a051d9929a776f808eca6e1a93af83313ca8a12
HARSHVARANDANI <hpvarandani@gmail.com>
2025-03-03 14:58:42 +0530

    middleware decryption fixed to use proper key and iv for the decryption process of AES-256-CBC

--- master_server/src/middlewares/decryption.middleware.js
+++ master_server/src/middlewares/decryption.middleware.js
@@ -7,8 +7,10 @@ export const decryptMiddleware = (req, res, next) => {
     if (!evmId) {
       return res.status(400).json({ message: "Missing evmId in headers" });
     }
+    console.log("req.body.data = ", req.body.data);
     // Assume req.body is an encrypted JSON string.
-    const decryptedData = decryptFromEVM(req.body, evmId);
+    const decryptedData = decryptFromEVM(req.body.data, evmId);
+    console.log("decryptedData = ", decryptedData);
     req.decryptedData = decryptedData;
     // Meaningless MITM catching
     // request to EVM health module
