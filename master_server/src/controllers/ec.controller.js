1e3f55770bb28b18e86b85a186d5250709713301
HARSHVARANDANI <hpvarandani@gmail.com>
2025-03-03 14:56:49 +0530

    handleecstaffregistration and handlevolunteerregistration in ec controller fixed to properly take input values from body of the post request and proper authentication method.

--- master_server/src/controllers/ec.controller.js
+++ master_server/src/controllers/ec.controller.js
@@ -6,6 +6,7 @@ import { formatResponse } from "../utils/formatApiResponse.js";
 // take admin biometrics
 export const handleEcStaffRegistration = async (req, res) => {
     const { id, name, biometric } = req.body;
+    console.log(req.body)
     try {
         const staffExist = await EC_Staff.findOne({
             where: {
@@ -27,7 +28,10 @@ export const handleEcStaffRegistration = async (req, res) => {
             biometric_left: encryptedLeftThumb
         });
 
-        return res.status(201).json(formatResponse(true, { message: "Staff created successfully" }, null, null));
+        const staffs = await EC_Staff.findAll();
+        console.log(staffs);
+
+        return res.status(201).json(formatResponse(true, { message: "Staff created successfully", staff: staff }, null, null));
     }
     catch (err) {
         console.error("Error during EC Staff registration:", err);
@@ -55,19 +59,28 @@ export const handleEcVolunteerRegistration = async (req, res) => {
         const encryptedRightThumb = encryptData(biometric.right);
         const encryptedLeftThumb = encryptData(biometric.left);
 
-        const staffMembers = await EC_Staff.findAll();
+        const staffProvided = await EC_Staff.findOne({ where: {
+            id: verifiedByStaff.id,
+        }})
 
-        let verifiedStaff = null;
+        let verifiedStaff = false;
 
-        for (const staff of staffMembers) {
-            const decryptedStaffRight = decryptData(staff.biometric_right);
-            const decryptedStaffLeft = decryptData(staff.biometric_left);
+        const decryptedStaffRight = decryptData(staffProvided.biometric_right);
+        const decryptedStaffLeft = decryptData(staffProvided.biometric_left)
 
-            if (decryptedStaffRight === verifiedByStaff || decryptedStaffLeft === verifiedByStaff) {
-                verifiedStaff = staff;
-                break;
-            }
+        if(verifiedByStaff.left === decryptedStaffLeft || verifiedByStaff.right === decryptedStaffRight) {
+            verifiedStaff = true;
         }
+
+        // for (const staff of staffMembers) {
+        //     const decryptedStaffRight = decryptData(staff.biometric_right);
+        //     const decryptedStaffLeft = decryptData(staff.biometric_left);
+
+        //     if (decryptedStaffRight === verifiedByStaff || decryptedStaffLeft === verifiedByStaff) {
+        //         verifiedStaff = staff;
+        //         break;
+        //     }
+        // }
 
         if (!verifiedStaff) {
             return res.status(404).json(formatResponse(false, null, 404, "Staff not found for verification."));
@@ -80,7 +93,7 @@ export const handleEcVolunteerRegistration = async (req, res) => {
             contact: contact,
             biometric_right: encryptedRightThumb,
             biometric_left: encryptedLeftThumb,
-            verifiedByStaff: verifiedStaff.id,
+            verifiedByStaff: verifiedByStaff.id,
         });
 
         return res.status(201).json(formatResponse(true, { message: "Volunteer created successfully", volunteer }, null, null));
